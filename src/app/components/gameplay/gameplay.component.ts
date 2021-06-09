0174d29fbd36e9d1a045fd3643aa1681502d692d
rubensandersonn <rubenssilva@great.local.ufc.br>
2021-06-09 13:29:31 -0300

    edges by rules

--- src/app/components/gameplay/gameplay.component.ts
+++ src/app/components/gameplay/gameplay.component.ts
@@ -24,6 +24,20 @@ export class GameplayComponent implements OnInit {
 
   mouseX: number;
   mouseY: number;
+  isMouseDown = false;
+
+  rules = [
+    [1, 2],
+    [0, 2],
+    [2, 3],
+    [1, 0],
+  ];
+  edges = [];
+
+  rectPoints = [new Point(500, 130), new Point(100, 100), new Point(300, 200), new Point(500, 300)];
+
+  rects: Rectangle[] = [];
+  lines: Line[] = [];
 
   constructor(
     private appService: AppService,
@@ -32,28 +46,17 @@ export class GameplayComponent implements OnInit {
     private router: Router
   ) {}
 
+  // rects must be setten before
+  setEdges() {
+    this.rules.forEach((el: number[]) => {
+      this.edges.push([new Point(this.rects[el[0]].cx, this.rects[el[0]].cy), new Point(this.rects[el[1]].cx, this.rects[el[1]].cy)]);
+    });
+
+    console.log(this.rules.length);
+  }
+
   ngOnInit(): void {
-    this.setupCanvas();
-
-    var rw = 100;
-    var rh = 50;
-
-    var x1 = 400;
-    var y1 = 10;
-    var x2 = 200;
-    var y2 = 200;
-
-    var cx1 = x1 + rw / 2;
-    var cy1 = y1 + rh / 2;
-    var cx2 = x2 + rw / 2;
-    var cy2 = y2 + rh / 2;
-
-    this.createRect(x1, y1, rw, rh);
-
-    this.createRect(x2, y2, rw, rh);
-
-    // line
-    this.createLine(cx1, cy1, cx2, cy2);
+    this.setupDragNDrop();
 
     /////////////////
     this.innerHeight = window.innerHeight * 0.6;
@@ -62,61 +65,83 @@ export class GameplayComponent implements OnInit {
     this.loadStates();
   }
 
-  dragElement(el: string, elx: number, ely: number) {
-    document.getElementById(el).onmousemove = this.mouseMove;
-    setInterval(() => this.moveElement(elx, ely), 100);
-  }
-
-  setupCanvas() {
+  setupDragNDrop() {
+    // setup canvas
     this.canvas = document.querySelector("canvas");
     this.canvas.height = window.innerHeight * 0.6;
     this.canvas.width = window.innerWidth * 0.92;
-
+    // setup ctx
     this.ctx = this.canvas.getContext("2d");
+
+    this.rectPoints.forEach((el, index) => {
+      let r = new Rectangle(el.x, el.y, 100, 50, this.canvas, this.ctx, "State");
+      this.rects.push(r);
+    });
+
+    this.setEdges();
+
+    this.edges.forEach((el, index) => {
+      let r = new Line(el[0].x, el[0].y, el[1].x, el[1].y, this.canvas, this.ctx, "label");
+      this.lines.push(r);
+    });
+
+    this.drawAll();
+
+    this.interact();
   }
 
-  createRect(x: number, y: number, w: number, h: number) {
-    this.ctx.fillStyle = "#52a798";
-    this.ctx.fillRect(x, y, w, h);
+  drawAll() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.lines.forEach((el, index) => {
+      el.draw();
+    });
+
+    this.rects.forEach((elm, index) => {
+      elm.draw();
+    });
   }
 
-  createLine(x1: number, y1: number, x2: number, y2: number) {
-    this.ctx.beginPath();
-    this.ctx.lineWidth = 3;
-    this.ctx.moveTo(x1, y1);
-    this.ctx.bezierCurveTo(x1, y2, x2, y1, x2, y2);
-    this.ctx.strokeStyle = "#232323";
-    this.ctx.stroke();
+  interact() {
+    document.getElementById("canvas").onmousemove = (evt: MouseEvent) => this.mouseMove(evt);
+    document.getElementById("canvas").onmousedown = (evt: MouseEvent) => this.mouseDown(evt);
+    document.getElementById("canvas").onmouseup = (evt: MouseEvent) => this.mouseUp(evt);
+    // setInterval(() => this.moveElement(elx, ely), 100);
   }
 
   ///////////////
   mouseMove(evt: MouseEvent) {
-    this.mouseX = evt.clientX;
-    this.mouseY = evt.clientY;
+    this.mouseX = evt.clientX - 50;
+    this.mouseY = evt.clientY - 176;
+
+    if (this.isMouseDown) {
+      console.log("is down", this.rects.length);
+      this.rects.forEach((el) => {
+        if (el.isTouching(this.mouseX, this.mouseY)) {
+          console.log("is touching", el.text, this.mouseX, this.mouseY, evt.clientX, evt.clientY);
+          el.update(this.mouseX, this.mouseY);
+        }
+      });
+    }
+    this.drawAll();
   }
 
-  moveElement(elx, ely) {
-    if (elx > this.mouseX) {
-      elx -= 5;
-    } else {
-      elx += 5;
-    }
-    if (ely > this.mouseY) {
-      ely -= 5;
-    } else {
-      ely += 5;
-    }
-
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-    this.ctx.beginPath();
-    this.ctx.arc(elx, ely, 40, 0, 2 * Math.PI);
-    this.ctx.fillStyle = "green";
-    this.ctx.fill();
-    this.ctx.lineWidth = 5;
-    this.ctx.strokeStyle = "red";
-    this.ctx.stroke();
+  mouseDown(evt: MouseEvent) {
+    this.isMouseDown = true;
+    console.log("mouse down", this.mouseX, this.mouseY, this.isMouseDown);
+    this.rects.forEach((el) => {
+      if (el.isTouching(this.mouseX, this.mouseY)) {
+        console.log("is touching", el.text, this.mouseX, this.mouseY, evt.clientX, evt.clientY);
+        el.update(this.mouseX, this.mouseY);
+      }
+    });
   }
+
+  mouseUp(evt: MouseEvent) {
+    this.isMouseDown = false;
+    console.log("mouse UP", this.mouseX, this.mouseY, this.isMouseDown);
+  }
+
   ///////////////
 
   clear() {
@@ -153,4 +178,113 @@ export class GameplayComponent implements OnInit {
   }
 
   ///////////////////
+}
+
+abstract class GeoForm {
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+  x: number;
+  y: number;
+  cx: number;
+  cy: number;
+  width: number = 100;
+  height: number = 50;
+  text: string;
+  color: string;
+  backgroundColor: string;
+
+  constructor(
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D,
+    text: string,
+    color?: string,
+    backColor?: string
+  ) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.width = w;
+    this.height = h;
+    this.x = x - this.width / 2;
+    this.y = y - this.height / 2;
+    this.text = text;
+    this.color = color != null ? color : "#707070";
+    this.backgroundColor = backColor != null ? backColor : "#52a798";
+    this.cx = x;
+    this.cy = y;
+  }
+
+  abstract draw(): void;
+
+  abstract isTouching(x: number, y: number): boolean;
+
+  update = (x: number, y: number) => {
+    if (x < this.width / 2 || x > this.canvas.width - this.width / 2 || y < this.height / 2 || y > this.canvas.height - this.height / 2) {
+      return;
+    }
+    this.x = x - this.width / 2;
+    this.y = y - this.height / 2;
+  };
+}
+
+class Rectangle extends GeoForm {
+  draw = () => {
+    this.ctx.fillStyle = this.backgroundColor != null ? this.backgroundColor : "#52a798";
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  };
+
+  isTouching(x: number, y: number) {
+    console.log("is it touching? (x, this.x, exp)", x, this.x, this.x + this.width);
+    return (this.x <= x || x <= this.x + this.width) && (this.y <= y || y <= this.y + this.height);
+  }
+}
+
+class Line extends GeoForm {
+  constructor(
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D,
+    text: string,
+    color?: string,
+    backColor?: string
+  ) {
+    super(x, y, w, h, canvas, ctx, text, color, backColor);
+    this.width = w;
+    this.height = h;
+    this.x = x;
+    this.y = y;
+    this.text = text;
+    this.color = color != null ? color : "#707070";
+    this.backgroundColor = backColor != null ? backColor : "#52a798";
+    this.cx = x + this.width / 2;
+    this.cy = y + this.height / 2;
+  }
+  draw = () => {
+    this.ctx.fillStyle = this.backgroundColor != null ? this.backgroundColor : "#52a798";
+    this.ctx.beginPath();
+    this.ctx.lineWidth = 3;
+    this.ctx.moveTo(this.x, this.y);
+    this.ctx.bezierCurveTo(this.x, this.height, this.width, this.y, this.width, this.height);
+    this.ctx.strokeStyle = this.color;
+    this.ctx.stroke();
+  };
+
+  isTouching(x: number, y: number) {
+    return false;
+  }
+}
+
+class Point {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
 }
